a9602f033f06aa3c7117d346c1107f9cf10f3f29
jamezzz123 <chinwokwuj@gmail.com>
2023-05-07 01:10:55 +0100

    refactor: adding stying to package

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+import './scss/custom.scss'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
